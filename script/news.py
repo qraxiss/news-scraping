47c57adb792687f7599fb0db65bf3121a60e3f2a
qraxiss <qraxiss@gmail.com>
2023-05-18 12:18:13 +0300

    scrape base

---
+++ script/news.py
@@ -0,0 +1,37 @@
+from script.models.report import Report
+from config import config
+
+from time import sleep
+
+from api.report import report
+from api.error import error
+
+class NewsScraper:
+    link = config.TARGET
+
+    last_report : Report
+    temp_report : Report
+
+
+    def get_last_report(self) -> Report:
+        ...
+
+    @property
+    def is_new(self) -> bool:
+        if self.last_report != self.temp_report:
+            self.last_report = self.temp_report
+            return True
+        else:
+            return False
+        
+
+    def connect_news(self):
+        while True:
+            try:
+                self.temp_report = self.get_last_report()
+            except Exception as e:
+                error(e)
+            else:
+                if self.is_new:
+                    report(self.last_report)
+            sleep(60)
