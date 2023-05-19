b7d465bb2b437899ce825baf09f1a31ce48dca2a
qraxiss <qraxiss@gmail.com>
2023-05-19 11:17:56 +0300

    Revert "Revert "Merge pull request #3 from stocksensai/trading-view""
    This reverts commit 4f0d1f335984cd4e5c3112711f0607a5a09b8030.

--- api/error.py
+++ api/error.py
@@ -2,7 +2,7 @@ from api.connection import interface
 def error(error : Exception):
     return interface.request(["send-message"], method="post", json={
         "text": f"""
-Message : {type(error).__name__}
-Error : {str(error)}
+🟥Error : {type(error).__name__}
+📥Message : {str(error)}
 """
     })
