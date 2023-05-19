ad0e0c8b4d93721eab96c7f06829a243224e5687
qraxiss <qraxiss@gmail.com>
2023-05-19 10:49:38 +0300

    error message template

--- api/error.py
+++ api/error.py
@@ -1,2 +1,8 @@
-def error():
-    ...
+from api.connection import interface
+def error(error : Exception):
+    return interface.request(["send-message"], method="post", json={
+        "text": f"""
+Message : {type(error).__name__}
+Error : {str(error)}
+"""
+    })
