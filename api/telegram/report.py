167091918ccefcc43d725206b211aa5bbee130ec
qraxiss <qraxiss@gmail.com>
2023-06-02 09:39:05 +0300

    api -> telegram
    telegram api moved to telegram folder

--- api/telegram/report.py
+++ api/telegram/report.py
@@ -1,10 +1,12 @@
 from script.models.report import Report
-from api.connection import interface
+from api.telegram.connection import interface
 
-def report_to_text(report: Report)->str:
+
+def report_to_text(report: Report) -> str:
     return f"◼{report.title}\n🔗{report.link}"
 
-def report(report : Report):
+
+def report(report: Report):
     return interface.request(["send-message"], method="post", json={
         "text": report_to_text(report)
     })
