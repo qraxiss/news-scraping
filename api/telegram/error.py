7ea65aafdd42e1adca0a81e658dfd236c18a8216
qraxiss <qraxiss@gmail.com>
2023-06-29 20:30:05 +0300

    route update v2

--- api/telegram/error.py
+++ api/telegram/error.py
@@ -2,7 +2,7 @@ from api.telegram.connection import interface
 
 
 def error(error: Exception):
-    return interface.request(["send-message"], method="post", json={
+    return interface.request(["message"], method="post", json={
         "text": f"""
 🟥Error : {type(error).__name__}
 📥Message : {str(error)}
